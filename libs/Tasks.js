826a883a28b60b5dd1d037de4584e280cfa5abd7
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-07-22 15:41:18 +0200

    working task display

--- libs/Tasks.js
+++ libs/Tasks.js
@@ -1,10 +1,5 @@
 
 
-function status(string){
-	var element = document.getElementById("status");
-	element.innerHTML=string;
-}
-
 function addTask(taskname){
 	var accordion = document.getElementById("accordion");
 	var taskentry = document.createElement("h3");
@@ -14,63 +9,27 @@ function addTask(taskname){
 	taskentry.appendChild(document.createTextNode(taskname));
 }
 
-function insertTask(content) {
+function insertTasks(content) {
     //document.getElementById('output').innerHTML = content;
 	//document.getElementById("tasksjson").appendChild(document.createTextNode("callback"));
-	console.log(content);
-    json = JSON.parse(content);
-    
-    document.getElementById("tasksjson").innerHTML="json";
-	//document.getElementById("tasksjson").innerHTML=json.toSource();
-	//.innerHTML=json.toSource();
+	console.log(content.task_names[0]);
+	content.task_names.forEach(function(elem){
+		console.log(elem);
+		addTask(elem)
+	});
+	//load the accordion UI Template
+	$( "#accordion" ).accordion();
 };
 
-//http://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url/2499647#2499647
-function getJSONP(url, success) {
 
-    var ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0] 
-               || document.documentElement;
-
-    window[ud] = function(data) {
-        head.removeChild(script);
-        success && success(data);
-    };
-
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
-
-}
-  
 
 function loadTasks(){
 	console.log( "loadTasks" );
+//	console.log( tasksResponse() );
 	var jsonloader = jQuery.getJSON("http://localhost:9292/tasks");
 	jsonloader.done(function(data){
-		console.log( "complete");
-		insertTask(data);
-		console.log( "second complete" );
-	});
-//	getJSONP('http://localhost:9292/tasks', function(data){
-//	    console.log(data);
-//	});
-//	tasks = document.createElement('a');
-//	tasks.href = "http://localhost:9292/tasks"
-//	taskelem = document.getElementById("tasks_json");
-//	taskelem.appendChild(document.createTextNode(JSON.stringify(tasks)));
-//	taskelem.appendChild(tasks);
-//		
-	//loadUrl('http://localhost:9292/tasks')
-	
-	addTask("test");
-	
-	addTask("test2");
-	
-	
+		insertTasks(data);
+	});	
 };
 
-function loadTasksWs(){
-	console.log( "loadTasksWs" );
-}
 
--- libs/WebSock.js
+++ libs/WebSock.js
@@ -1,38 +1,26 @@
 
 
-function status(string){
-	var element = document.getElementById("status");
-	element.innerHTML=string;
-}
+function WebSock(url, callback){
 
-function debug(string) {
-	var element = document.getElementById("debug");
-	var p = document.createElement("p");
-	p.appendChild(document.createTextNode(string));
-	element.appendChild(p);
-};
-
-function WebSock(ip, port){
-
-	var ws = new WebSocket("ws://"+ip+":"+port);
+	var ws = new WebSocket(url);
 	ws.onmessage = function(evt) { 
-		status(evt.data);	
+		console.log(evt.data);
+		callback(evt.data)
 	};
 	ws.onclose = function(event) {
-		debug("Closed - code: " + event.code + ", reason: " + event.reason + ", wasClean: " + event.wasClean);
-		var obj = document.getElementById("connbtn");
-		obj.className="red";
-		obj.innerHTML="disconnected";
+		console.log("Closed - code: " + event.code + ", reason: " + event.reason + ", wasClean: " + event.wasClean);
+		//var obj = document.getElementById("connbtn");
+		//obj.className="red";
+		//obj.innerHTML="disconnected";
 	};
 	ws.onopen = function() {
-		//debug("connected...");
-		var obj = document.getElementById("connbtn");
-		obj.className="green";
-		obj.innerHTML="connected";
+		console.log("connected...");
+//		var obj = document.getElementById("connbtn");
+//		obj.className="green";
+//		obj.innerHTML="connected";
 	};
 
 	this.send = function(message) {
-		//debug("send " + message);
 		ws.send(message);
 	};
 
