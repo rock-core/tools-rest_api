bc373b81b43d98c72bb4501f2e9c924c8d092906
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-08-13 12:13:11 +0200

    added gui.js and .css to create collapsable parts

---
+++ libs/GUI.js
@@ -0,0 +1,49 @@
+
+//http://stackoverflow.com/questions/17460116/expand-and-collapse-a-div-using-javascript
+
+
+
+function createCollapsable(content, openname, closename){
+	var outer = document.createElement("div");
+	outer.setAttribute("class","collapsablecontainer");
+	
+	var head = document.createElement("div");
+	head.setAttribute("class","collapsableheader");
+	head.innerHTML = openname;
+	
+	
+	var coll = document.createElement("div");
+	coll.setAttribute("class","collapsablecontent");
+	
+	outer.appendChild(head);
+	startCollapsable(head, openname, closename);
+	outer.appendChild(coll);
+	coll.appendChild(content);
+	
+	
+	return outer;
+}
+
+
+//to be called after adding to doc, change click behavior
+function startCollapsable(element, openname, closename){
+	$(element).click(function () {
+		console.log("coll");
+		$header = $(this);
+		//getting the next element
+		$content = $header.next();
+		//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+		$content.toggle(500, function () {
+			//execute this after slideToggle is done
+			//change text of header based on visibility of content div
+			$header.text(function () {
+				//change text based on condition
+				return $content.is(":visible") ? closename : openname;
+			});
+		});
+	
+	});
+}
+
+
+
