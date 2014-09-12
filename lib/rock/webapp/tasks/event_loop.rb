19e4c4d041a83dbf0c29d13d1de8e19ad3738857
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-09-12 17:11:23 +0200

    use EM callbacks to delete expired writers

--- lib/rock/webapp/tasks/event_loop.rb
+++ lib/rock/webapp/tasks/event_loop.rb
@@ -13,15 +13,7 @@ module Rock
                 EM.cancel_timer(@async_event_loop_timer)
                 @async_event_loop_timer = nil
             end
-            
-            def self.install_port_writer_clean_loop(period = 5)
-                @port_writer_clean_loop_timer ||=
-                    EM.add_periodic_timer period do
-                        TasksAPI.port_writers.clean
-                    end
-            end
-            
-            
+
         end
     end
 end
