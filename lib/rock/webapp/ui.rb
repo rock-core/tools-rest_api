7c0e48b1a07128ee6c0c73fcbf1e4210b972ca94
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-09-12 15:33:36 +0200

    moved root.rb files one higher and changed name to the original folder

---
+++ lib/rock/webapp/ui.rb
@@ -0,0 +1,15 @@
+
+module Rock
+    module WebApp
+        module UI
+
+            LIB_DIR = File.expand_path(File.dirname(__FILE__))
+            DIR  = File.join(LIB_DIR, 'ui', 'www')
+            
+            if ENV['ROCK_WEBAPP_CUSTOM_PATH']
+                ADDON_DIR = File.expand_path(ENV['ROCK_WEBAPP_CUSTOM_PATH'])    
+            end
+        end
+    end
+end
+
