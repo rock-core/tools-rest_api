82e094a27ce95b5208b2c52f6c92c87b089929b3
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-09-12 17:17:12 +0200

    moved www folder content one up into ui

--- lib/rock/webapp/ui.rb
+++ lib/rock/webapp/ui.rb
@@ -4,7 +4,7 @@ module Rock
         module UI
 
             LIB_DIR = File.expand_path(File.dirname(__FILE__))
-            DIR  = File.join(LIB_DIR, 'ui', 'www')
+            DIR  = File.join(LIB_DIR, 'ui')
             
             if ENV['ROCK_WEBAPP_CUSTOM_PATH']
                 ADDON_DIR = File.expand_path(ENV['ROCK_WEBAPP_CUSTOM_PATH'])    
