2b9c3d2c96376e49c9c9b1f40c40ba5389af35d9
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-10-27 11:12:44 +0100

    fixed timer logic (timeout times), proper handling of ports without timeout (manual delete by websocket.disconnect)

--- lib/rock/webapp/tasks/cached_ports.rb
+++ lib/rock/webapp/tasks/cached_ports.rb
@@ -4,30 +4,36 @@ module Rock
             class CachedPorts
                 
                 def initialize
-                    @portentries = {} 
+                    @portentries = {}
+                    @timer = nil
                 end
                                         
                 class PortEntry
+                    attr_accessor :timer
                     
                     #if lifetiem_s ==0, there will be no timeout
-                    def initialize(port, lifetime_seconds, init=false)
-                        @timestamp = Time.now().to_i
-                        @lifetime_s = lifetime_seconds
+                    def initialize(port, timeout, init=false)
+                        
+                        @end_lifetime = Time.at(Time.now.to_i + timeout.to_i)
+                        
+                        #puts "new port will time out at #{@end_lifetime}"
+                        
                         if port.respond_to?(:writer)
                             @writer = port.writer
                         end
                         if port.respond_to?(:reader)
                             @reader = port.reader(init: init, pull: true)
                         end
+                        
                         @port = port
                     end
                     
                     def is_writer?
-                        return @port.respond_to?(:writer)
+                        @port.respond_to?(:writer)
                     end
                     
                     def is_reader?
-                        return @port.respond_to?(:reader)
+                        @port.respond_to?(:reader)
                     end
                     
                     def name
@@ -46,22 +52,29 @@ module Rock
                         @port
                     end 
                     
+                    def timer=(value)
+                        @timer=value
+                    end
+                    
                     def write(obj, timeout)
                         if self.connected?
-                            if timeout > @lifetime_s
-                                @lifetime_s = timeout
+                            new_end = Time.at(Time.now.to_i + timeout.to_i)
+                            #puts "old:\t#{@end_lifetime}\nnew:\t#{new_end}"
+                            if new_end > @end_lifetime
+                                @end_lifetime = new_end
+                                #puts "existing write port will now time out at #{@end_lifetime}"
                             end
-                            @timestamp = Time.now
                             @writer.write(obj)
                         end
                     end
                     
                     def read(obj, timeout)
                         if self.connected?
-                            if timeout > @lifetime_s
-                                @lifetime_s = timeout
+                            new_end = Time.at(Time.now.to_i + timeout.to_i)
+                            if new_end > @end_lifetime
+                                @end_lifetime = new_end
+                                #puts "existing read port will now time out at #{@end_lifetime}"
                             end
-                            @timestamp = Time.now
                             @reader.read(obj)
                         end
                     end
@@ -74,58 +87,84 @@ module Rock
                         if reader_or_writer.connected?
                             return true
                         else
-                            @lifetime_s = 0
+                            @end_lifetime = Time.at(0) #start of epoch, definately tiimed out
                             return false
                         end
                     end
                     
                     def lifetime_left
-                        @lifetime_s - (Time.now - @timestamp).to_i  
+                        return @end_lifetime - Time.now
                     end
                     
                 end
                             
-                def add(port, name_service, name, port_name, init = false, lifetime_seconds = Float::INFINITY)
-                    #puts "added writer with #{lifetime_seconds} timeout"
-                    entry = PortEntry.new(port, lifetime_seconds, init)
+                #timeout == 0 won't start a timer to delete the port
+                def add(port, name_service, name, port_name, init = false, timeout)
+                    #puts "added writer with #{timeout} timeout"
+                    entry = PortEntry.new(port, timeout, init)
                     key = name_service+name+port_name;
                     @portentries[key] = entry
                     #puts "add writer size: #{@writers.length}"
-                    if lifetime_seconds < Float::INFINITY
-                        create_timed_delete(lifetime_seconds, key)
+                    if timeout > 0
+                        create_timed_delete(timeout, key)
                     end
                     entry
                 end
                 
-                def get(name_service, name, port_name)
+                #timeout == 0 will cancel the running EM timer (re-started at delete)
+                def get(name_service, name, port_name, timeout)
                     key = name_service+name+port_name
-                    writer = @portentries[key]
-                    if writer && writer.connected?
-                        return writer 
+                    port = @portentries[key]
+                    if port && port.connected?
+                        if timeout == 0
+                            #puts "cancelling timer"
+                            EM.cancel_timer(port.timer)
+                            port.timer=nil
+                        end
+                        return port 
                     end
                     @portentries.delete(key)
                     nil
                 end
         
-                def create_timed_delete(lifetime_seconds, key)
-                    EM.add_timer(lifetime_seconds) do
-                        writer = @portentries[key]
-                        if writer #the wruiter moght have benn already deleted because it was disconnected
-                            time_left = writer.lifetime_left
+                #can take Interger of Time as timeout arg
+                def create_timed_delete(timeout, key)
+                    mtimer = EM.add_timer(timeout.to_i) do
+                        port = @portentries[key]
+                        if port #the writer might have been already deleted because it was disconnected
+                            time_left = port.lifetime_left
                             if time_left > 0
-                                #puts "object has #{time_left} seconds left"
+                                #puts "object has #{time_left} seconds left, #{Time.now}"
                                 create_timed_delete(time_left, key)
                             else
-                                #puts "deleting writer"
+                                #puts "timed delete writer #{Time.now}"
                                 @portentries.delete(key)
                             end
                         end
                     end
+                    port = @portentries[key]
+                    port.timer = mtimer
                 end 
                 
+                # don't delete if there is lifetime left
+                # this may happen, when the port was initially created withpout lifetime (user delete)
+                # but used by another part with a timeout
+                def soft_delete(name_service, name, port_name)
+                    key = name_service+name+port_name
+                    #puts key + "soft deleting #{Time.now}"
+                    port = @portentries[key]
+                    if port && port.lifetime_left > 0
+                        create_timed_delete(port.lifetime_left, key)
+                        #puts key + "soft deleting #{Time.now} new timer #{port.lifetime_left}"
+                    else
+                        @portentries.delete(key)
+                    end
+                end
+                
+                #force-delete a port, no matter if there are pending timeouts
                 def delete(name_service, name, port_name)
                     key = name_service+name+port_name
-                    #puts key + "disconnected, deleting"
+                    #puts key + "deleting #{Time.now}"
                     @portentries.delete(key)
                 end
                 
