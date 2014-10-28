4fb3f1ad8d499d7d7b6486dcf669413f75cff669
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-10-28 08:47:33 +0100

    removed puts from syskit api

--- lib/rock/webapp/syskit/api.rb
+++ lib/rock/webapp/syskit/api.rb
@@ -26,10 +26,7 @@ module Rock
                     end
                     
                     post ':action/start' do
-                        puts "start #{params.values_at('action')}"
-                        puts request.params.pretty_inspect
                         mparams = MultiJson.load(request.params["value"])
-                          puts mparams
                         interface.start_action(params[:action],mparams)
                     end
                      
@@ -43,18 +40,13 @@ module Rock
                     end 
                     
                     post 'killall' do
-                      puts "killall"
                         interface.killall
                     end
                     
                     desc "kill a job"
 
                     post 'kill' do
-                      puts "kill"
-                      puts request.params
                       mparams = MultiJson.load(request.params["value"])
-                      puts mparams.pretty_inspect
-                      puts mparams["id"]
                       interface.kill(mparams["id"].to_i)
                     end
                     
