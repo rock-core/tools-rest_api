e3b7c823d6dc6af4de7aa1b7f57fb71da94f8275
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2015-05-19 14:28:54 +0200

    fixed port selection

--- lib/rock/webapp/tasks/api.rb
+++ lib/rock/webapp/tasks/api.rb
@@ -260,7 +260,7 @@ module Rock
                     
                     desc 'disconnect a port completely'
                     get ':name_service/:name/ports/:port_name/disconnect_all' do
-                        port = port_by_task_and_name(*params.values_at('name_service', 'from', 'port'))
+                        port = port_by_task_and_name(*params.values_at('name_service', 'name', 'port_name'))
                         port.disconnect_all    
                     end
                     
